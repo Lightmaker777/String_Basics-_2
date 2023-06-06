aedf8b224246c4e337e403dc449ba7a2045843f2
Lightmaker777 <j.kochkorian@gmx.de>
2023-06-06 17:13:46 +0200

    Added 2nd exercise

---
+++ python_text_basic_2.py
@@ -0,0 +1,85 @@
+
+# Task 1
+
+text = """Berlin is a world city of culture, politics, media and science."""
+
+print(len(text),"\n")
+
+# Task 2
+
+text = """Berlin is a world city of culture, politics, media and science."""
+
+print(text[0],text[-1])
+
+# Task 3
+
+text = """Berlin is a world city of culture, politics, media and science."""
+
+print("\nFirst three characters: " + text[0:3].upper() + "\n")
+
+# Task 4
+
+text = """Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital"""
+
+print("B appears:", text.count("B"), "times\n")
+
+# Task 5
+
+text = """Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."""
+print("Last ten characters:", text[-10:],"\n")
+
+# Task 6
+
+text = "---Python programming---"
+
+print(text.strip("-"),"\n")
+
+# Task 7
+
+firstname = "Ievgeniia"
+lastname = "Ponomarenko"
+
+print("Firstname:", firstname,"\nLastname:",lastname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
